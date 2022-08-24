2e06002f883506ffae9d84d55559e317c07003b0
PuneetChaubey <puneeteklavya@gmail.com>
2022-08-24 21:41:25 +0530

    created Dasboard route

--- Client/src/Pages/MainRoutes.jsx
+++ Client/src/Pages/MainRoutes.jsx
@@ -3,6 +3,7 @@
 
 import React from 'react'
 import {Routes, Route} from "react-router-dom"
+import Dashboard from './Dashboard'
 import HomePage from './HomePage'
 import Login from './Login'
 import Project from './Project'
@@ -11,7 +12,8 @@ const MainRoutes = () => {
   return (
       <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/projects"  element={<Project/>}/>
+      <Route path="/projects" element={<Project />} />
+      <Route path="/dashboard" element={<Dashboard/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/signup' element={<Signup/>}/>
     </Routes>
