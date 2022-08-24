1e3d97d6b3dd8cab341eb948e11d98b631c8fb07
PuneetChaubey <puneeteklavya@gmail.com>
2022-08-24 13:34:50 +0530

    done around 50% of Projectheaders

--- Client/src/Pages/MainRoutes.jsx
+++ Client/src/Pages/MainRoutes.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Routes, Route} from "react-router-dom"
 import HomePage from './HomePage'
+import Project from './Project'
 const MainRoutes = () => {
   return (
       <Routes>
-          <Route path="/" element={<HomePage/>}/>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/projects" element={<Project/>}/>
     </Routes>
   )
 }
