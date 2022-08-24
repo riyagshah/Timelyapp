902cced8785dd79e121074ae373fe64b23365420
Shashank Nath <shashankfeb16@gmail.com>
2022-08-24 21:01:24 +0530

    day 2 frontend

--- Client/src/Pages/MainRoutes.js
+++ Client/src/Pages/MainRoutes.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import {Routes, Route} from "react-router-dom"
 import HomePage from './HomePage'
+import Login from './Login'
+import Signup from './Signup'
 const MainRoutes = () => {
   return (
       <Routes>
           <Route path="/" element={<HomePage/>}/>
+          <Route path='/login' element={<Login></Login>}></Route>
+          <Route path='/signup' element={<Signup></Signup>}></Route>
     </Routes>
   )
 }
