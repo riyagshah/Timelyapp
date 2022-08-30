8c2dc03c9440e0eb2195876f143dc7dc7889592d
PuneetChaubey <puneeteklavya@gmail.com>
2022-08-30 13:33:10 +0530

    added localstorge to store token and created protected route

--- Client/src/Pages/MainRoutes.jsx
+++ Client/src/Pages/MainRoutes.jsx
@@ -10,17 +10,46 @@ import Signup from "./Signup";
 import DayCalender from "../Components/calender/DayCalender";
 import WeekCalender from "../Components/calender/WeekCalender";
 import Calender from "../Components/calender/MonthCalender";
+import RequiredAuth from "../HOC/RequiredAuth";
 const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/projects" element={<Project />} />
-     
-      <Route path='/day' element={<DayCalender/>}/>
-       <Route path='/week' element={<WeekCalender/>}/>
-       <Route path='/month' element={<Calender/>}/>
+      <Route
+        path="/projects"
+        element={
+          <RequiredAuth>
+            <Project />
+          </RequiredAuth>
+        }
+      />
+
+      <Route
+        path="/day"
+        element={
+          <RequiredAuth>
+            <DayCalender />
+          </RequiredAuth>
+        }
+      />
+      <Route
+        path="/week"
+        element={
+          <RequiredAuth>
+            <WeekCalender />
+          </RequiredAuth>
+        }
+      />
+      <Route
+        path="/month"
+        element={
+          <RequiredAuth>
+            <Calender />
+          </RequiredAuth>
+        }
+      />
       <Route path="/sales" element={<Sales />} />
-      
+
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
     </Routes>
--- Client/src/Redux/AuthReducer/reducer.js
+++ Client/src/Redux/AuthReducer/reducer.js
@@ -1,17 +1,19 @@
+import { getLocalData, saveLocalData } from "../../utils/LocalStorage";
 import { LOGIN_ERROR, LOGIN_LOADING, LOGIN_SUCCESS } from "./actionType";
 
 const initialState = {
     loading: false,
     error: false,
-    isAuth: false,
-    token: ""
+    isAuth: getLocalData("token")? true : false,
+    token: getLocalData("token") || ""
 }
 const reducer = (state=initialState, {type,payload}) => {
     switch (type){
         case LOGIN_LOADING :{
             return {...state,loading:true,error:false}
         }
-        case LOGIN_SUCCESS:{
+        case LOGIN_SUCCESS: {
+            saveLocalData("token", payload.token);
             return {...state,loading:false,error:false,isAuth:true,token:payload.token}
         }
         case LOGIN_ERROR:{
